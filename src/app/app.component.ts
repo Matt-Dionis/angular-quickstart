ad041cf94c3dfd053e93a49449bdcdff89efb568
Matt Dionis <mattdionis@MattBook-Pro.local>
2017-04-20 14:56:42 -0400

    Update message and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Our first Angular {{1 + 1}} component!</h1>`,
 })
 export class AppComponent  { name = 'Angular'; }
