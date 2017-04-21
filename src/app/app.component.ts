1398c1caa023322646879e76b1261c648e461778
Matt Dionis <mattdionis@MattBook-Pro.local>
2017-04-21 08:06:02 -0400

    Remove unnecessary 'name' variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,4 +4,4 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `<h1>Our first Angular {{1 + 1}} component!</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { }
